b7d4753005e0fa7f5e0df5eb4720af1f1c622442
Dheeraj Kumar <55842638+Dheeraj12121@users.noreply.github.com>
2022-05-30 18:50:47 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,29 +3,7 @@ import React from 'react'
 import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
 import { Carousel } from 'react-responsive-carousel'
 const Header = () => {
-  const imges = [
-    {
-      img: 'https://nptel.ac.in/assets/shared/carousel/Office.jpg',
-    },
-    {
-      img: 'https://nptel.ac.in/assets/shared/carousel/NPTEL%20Merchandise.jpg',
-    },
-    {
-      img: 'https://nptel.ac.in/assets/shared/carousel/NPTEL%20Stars.jpg',
-    },
-    {
-      img: 'https://nptel.ac.in/assets/shared/carousel/Course%20offering%20faculty.jpg',
-    },
-    {
-      img: 'https://nptel.ac.in/assets/shared/carousel/Special%20Live%20Series.jpg',
-    },
-    {
-      img: 'https://nptel.ac.in/assets/shared/carousel/Workshop.jpg',
-    },
-    {
-      img: 'https://nptel.ac.in/assets/shared/carousel/Coordinator.jpg',
-    },
-  ]
+  
   return (
     <div className="-z-50 mb-4">
       <Carousel showStatus="" infiniteLoop autoPlay showThumbs="" stopOnHover>
@@ -45,7 +23,7 @@ const Header = () => {
           <img src="https://nptel.ac.in/assets/shared/carousel/Course%20offering%20faculty.jpg" alt="" />
         </div>
         <div>
-          <img src="" alt="https://nptel.ac.in/assets/shared/carousel/Special%20Live%20Series.jpg" />
+          <img src="https://nptel.ac.in/assets/shared/carousel/Special%20Live%20Series.jpg" alt="" />
         </div>
         <div>
           <img src="https://nptel.ac.in/assets/shared/carousel/Workshop.jpg" alt="" />

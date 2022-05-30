18a9b41ac6b9824f23f066ee1a4e6b382c0a2442
Dheeraj Kumar <dkreal50@gmail.com>
2022-05-30 18:39:45 +0530

    updated

--- components/Header.js
+++ components/Header.js
@@ -1,30 +1,59 @@
 import Image from 'next/image'
 import React from 'react'
-import Carousel from 'react-elastic-carousel'
-import Item from './Item'
-
+import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
+import { Carousel } from 'react-responsive-carousel'
 const Header = () => {
+  const imges = [
+    {
+      img: 'https://nptel.ac.in/assets/shared/carousel/Office.jpg',
+    },
+    {
+      img: 'https://nptel.ac.in/assets/shared/carousel/NPTEL%20Merchandise.jpg',
+    },
+    {
+      img: 'https://nptel.ac.in/assets/shared/carousel/NPTEL%20Stars.jpg',
+    },
+    {
+      img: 'https://nptel.ac.in/assets/shared/carousel/Course%20offering%20faculty.jpg',
+    },
+    {
+      img: 'https://nptel.ac.in/assets/shared/carousel/Special%20Live%20Series.jpg',
+    },
+    {
+      img: 'https://nptel.ac.in/assets/shared/carousel/Workshop.jpg',
+    },
+    {
+      img: 'https://nptel.ac.in/assets/shared/carousel/Coordinator.jpg',
+    },
+  ]
   return (
-    <div className='mb-4 -z-50'>
-      <Carousel enableAutoPlay autoPlaySpeed={1500}>
-        <Item>
-          <img src="https://nptel.ac.in/assets/shared/carousel/Coordinator.jpg" alt="" />
-        </Item>
-        <Item>
-        <img src="https://nptel.ac.in/assets/shared/carousel/NPTEL%20Merchandise.jpg" alt="" />
-        </Item>
-        <Item>
+    <div className="-z-50 mb-4">
+      <Carousel showStatus="" infiniteLoop autoPlay showThumbs="" stopOnHover>
+        <div>
+          <img
+            src={'https://nptel.ac.in/assets/shared/carousel/Office.jpg'}
+            alt=""
+          />
+        </div>
+        <div>
+          <img src="https://nptel.ac.in/assets/shared/carousel/NPTEL%20Merchandise.jpg" alt="" />
+        </div>
+        <div>
           <img src="https://nptel.ac.in/assets/shared/carousel/NPTEL%20Stars.jpg" alt="" />
-        </Item>
-        <Item>
+        </div>
+        <div>
           <img src="https://nptel.ac.in/assets/shared/carousel/Course%20offering%20faculty.jpg" alt="" />
-        </Item>
-        <Item>
-          <img src="https://nptel.ac.in/assets/shared/carousel/Special%20Live%20Series.jpg" alt="" />
-        </Item>
-        <Item>
+        </div>
+        <div>
+          <img src="" alt="https://nptel.ac.in/assets/shared/carousel/Special%20Live%20Series.jpg" />
+        </div>
+        <div>
+          <img src="https://nptel.ac.in/assets/shared/carousel/Workshop.jpg" alt="" />
+        </div>
+        <div>
           <img src="https://nptel.ac.in/assets/shared/carousel/Coordinator.jpg" alt="" />
-        </Item>
+        </div>
+
       </Carousel>
     </div>
   )
